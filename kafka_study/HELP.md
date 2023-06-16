c44b0f5bec3bca6c542f3126b726f3224913e21a
xia shengchun <xsc19850920@163.com>
2023-06-16 22:10:44 +0800

    增加kafka 模块

--- kafka_study/HELP.md
+++ kafka_study/HELP.md
@@ -1,3 +1,4 @@
 参考文档
 
 https://github.com/Snailclimb/springboot-kafka
+https://blog.csdn.net/FBB360JAVA/article/details/129603572
--- kafka_study/src/main/java/com/sxia/service/KafkaService.java
+++ kafka_study/src/main/java/com/sxia/service/KafkaService.java
@@ -2,7 +2,6 @@ package com.sxia.service;
 
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
